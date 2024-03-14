b0cda5ef7e4d3df2be160d5cab0992d083020bd9
Michael <michael@Zipcoders-MacBook-Pro-4.local>
2024-03-13 21:15:56 -0400

    Finished

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,62 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+
+    private Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
+
     public Boolean isInFavorOfX() {
-        return null;
+        for (int i = 0; i < 3; i++){
+            if (matrix[0][i].equals('X') && matrix[1][i].equals('X') && matrix[2][i].equals('X')) {
+                return true;
+            } else if (matrix[i][0].equals('X') && matrix[i][1].equals('X') && matrix[i][2].equals('X')) {
+                return true;
+            }
+        }if (matrix[0][0].equals('X') && matrix[1][1].equals('X') && matrix[2][2].equals('X')){
+            return true;
+        } else if (matrix[2][0].equals('X') && matrix[1][1].equals('X') && matrix[0][2].equals('X')) {
+            return true;
+        }
+
+        return false;
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+        for (int i = 0; i < 3; i++){
+            if (matrix[0][i].equals('O') && matrix[1][i].equals('O') && matrix[2][i].equals('O')) {
+                return true;
+            } else if (matrix[i][0].equals('O') && matrix[i][1].equals('O') && matrix[i][2].equals('O')) {
+                return true;
+            }
+        }if (matrix[0][0].equals('O') && matrix[1][1].equals('O') && matrix[2][2].equals('O')){
+            return true;
+        } else if (matrix[2][0].equals('O') && matrix[1][1].equals('0') && matrix[0][2].equals('O')) {
+            return true;
+        }
+
+        return false;
     }
 
     public Boolean isTie() {
-        return null;
+        if (isInFavorOfO() != true && isInFavorOfX() != true){
+            return true;
+        }
+        return false;
     }
 
     public String getWinner() {
-        return null;
+        String winnerO = "O";
+        String winnerX= "X";
+        if (isInFavorOfO() == true) {
+            return winnerO;
+        }else if (isInFavorOfX() == true) {
+            return winnerX;
+        }
+        return "";
     }
 
 }
